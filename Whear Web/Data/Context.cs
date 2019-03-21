5cee3763776c795b7c1c7fd3a2c0c5a58e617af0
JB-d23jekweq <48787552+JB-d23jekweq@users.noreply.github.com>
2019-03-21 13:35:38 +0100

    Commentary

--- Whear Web/Data/Context.cs
+++ Whear Web/Data/Context.cs
@@ -8,24 +8,11 @@ namespace Whear.Web.Data
         public DbSet<User> User { get; set; }
         
         public Context(DbContextOptions<Context> options) : base(options) { }
-
-        /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseMySQL("server=localhost;database=whear;user=root;password=");
-        }*/
-
+        
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            //base.OnModelCreating(modelBuilder);
-
+            // Converts models to MySQL tables.
             modelBuilder.Entity<User>().ToTable("user");
-            
-            /*modelBuilder.Entity<User>(entity =>
-            {
-                entity.HasKey(e => e.ID);
-                entity.Property(e => e.Email);
-                entity.Property(e => e.Password);
-            });*/
         }
     }
 }
